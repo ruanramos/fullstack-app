ae113b011934d33fdc7cd1de2d2da73978d68826
ruanramos <ruan.ramos28@gmail.com>
2022-08-14 01:08:31 -0300

    Fixing adress requirement

--- frontend/src/types/Doctor.d.ts
+++ frontend/src/types/Doctor.d.ts
@@ -2,7 +2,7 @@ export type Doctor = {
     _id: number,
     name: string,
     specialty: string,
-    address: string,
+    address?: string,
     phone: string,
     createdAt: string,
     updatedAt: string
--- server/src/models/doctor.js
+++ server/src/models/doctor.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.model('Doctor', new mongoose.Schema({
     name: { type: String, required: true },
     specialty: { type: String, required: true },
-    address: { type: String, required: true },
+    address: { type: String, required: false },
     phone: { type: String, required: true },
 }, { timestamps: true }));
 
